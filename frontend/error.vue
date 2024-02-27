90fe8eaa47fccb54dca2e0d12a5d511aec6a6f79
Severin Rosner <einsevspielt@gmail.com>
2024-02-27 19:39:29 +0100

    feat: code setup

---
+++ frontend/error.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import type { NuxtError } from "#app";
+
+const props = defineProps({
+  error: Object as () => NuxtError,
+});
+</script>
+
+<template>
+  <div>
+    <h1 v-if="error">There was an error.</h1>
+    <h1 v-if="error">{{ error.statusCode }}</h1>
+    <NuxtLink to="/">Go back home</NuxtLink>
+  </div>
+</template>
