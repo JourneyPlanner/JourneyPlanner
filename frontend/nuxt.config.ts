b4393b80181ab9f555e0107bcf50b12fa50239fa
Ixpiria <roman.krebs@gmx.at>
2024-03-04 22:40:49 +0100

    JP-5: feat: (beginning of registration)

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -2,11 +2,16 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ["@/assets/css/fonts/fonts.css"],
-  modules: ["@nuxtjs/tailwindcss"],
+  modules: ["@nuxtjs/tailwindcss", "nuxt-primevue"],
   runtimeConfig: {
     public: {
       NUXT_TOLGEE_API_KEY: process.env.NUXT_TOLGEE_API_KEY,
       NUXT_TOLGEE_API_URL: process.env.NUXT_TOLGEE_API_URL,
+    },
+  },
+  primevue: {
+    components: {
+      include: ["Password"],
     },
   },
 });
