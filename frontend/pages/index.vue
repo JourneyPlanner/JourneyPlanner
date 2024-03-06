6226120adcc93ec926e1b7b3e1bec932c8422362
Severin Rosner <einsevspielt@gmail.com>
2024-03-06 11:04:59 +0100

    feat: add fonts

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex justify-center">
+  <div class="flex justify-center font-nunito">
     <div class="text-center mt-10">
       <h1 class="text-4xl font-bold">JourneyPlanner</h1>
     </div>
