9ff617c61c45ac9e169827531dbfc12de99bb645
Severin Rosner <einsevspielt@gmail.com>
2024-03-06 11:04:59 +0100

    feat: add fonts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -2,8 +2,24 @@
 export default {
   content: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        nunito: "Nunito",
+        "nunito-sans": "Nunito Sans",
+      },
+    },
+    colors: {
+      "text-black": "#212121",
+      background: "#ffffff",
+      primary: "#bad9e7",
+      secondary: "#d8eaf3",
+      "call-to-action": "#f8d351",
+      "placeholder-gray": "#212121",
+      "disabled-input": "#ECEAEA",
+      cancel: "#829094",
+      delete: "#F06543",
+    },
   },
   plugins: [],
-}
+};
 
