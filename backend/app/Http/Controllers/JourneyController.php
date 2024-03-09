201875ecc88ffcb04916716e89b5dc57e84b8a0c
Raven Burkard <mathematti@outlook.com>
2024-03-09 21:04:09 +0100

    JP-8: feat: Backend functions for journey creation (untested)

---
+++ backend/app/Http/Controllers/JourneyController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Journey;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+
+class JourneyController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        $validated = $request->validate(
+            [
+                'name' => 'required|string',
+                'destination' => 'required|string',
+                'from' => 'required|date',
+                'to' => 'required|date',
+                'invite' => 'required|uuid',
+            ]
+        );
+
+        $journey = Journey::create($validated);
+        $journey->users()->attach(auth()->id());
+
+        return response()->json(
+            [
+                'message' => 'Journey created successfully',
+                'journey' => $journey,
+            ],
+            201
+        );
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Journey $journey)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Journey $journey)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Journey $journey)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Journey $journey)
+    {
+        //
+    }
+}
