a0b380fbbd5288a85ada23845bd5da60d24e061e
Raven Burkard <mathematti@outlook.com>
2024-03-13 07:05:49 +0100

    JP-32: fix: More meaningful comments

--- backend/app/Http/Controllers/JourneyUserController.php
+++ backend/app/Http/Controllers/JourneyUserController.php
@@ -5,14 +5,15 @@ namespace App\Http\Controllers;
 use App\Models\Journey;
 use App\Models\JourneyUser;
 use Eloquent;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class JourneyUserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Get the users of the journey
      */
-    public function index($id)
+    public function index($id): JsonResponse
     {
         // check if the journey exists and if the user is a part of it
         $journey = Journey::find($id);
