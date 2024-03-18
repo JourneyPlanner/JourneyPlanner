61334ee898995ad878b793e1be9828b903b24af6
Ixpiria <roman.krebs@gmx.at>
2024-03-18 21:54:53 +0100

    JP-65: wip: id in url working

---
+++ frontend/pages/journey/[id].vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const route = useRoute();
+
+console.log(route.params.id);
+</script>
+
+<template>
+  <div class="w-2/3 h-screen flex justify-end">
+    <div class="w-2/3 rounded-xl bg-cta-bg h-10"></div>
+  </div>
+</template>
