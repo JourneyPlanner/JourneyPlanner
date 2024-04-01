17a2bc52dbd6e80fef621dac3a17396d6d98738b
Severin Rosner <einsevspielt@gmail.com>
2024-04-01 14:47:41 +0200

    JP-65: fix: add types for Calendar and Password value

--- frontend/components/form/Password.vue
+++ frontend/components/form/Password.vue
@@ -29,34 +29,20 @@ const props = defineProps({
   translationKey: { type: String, required: true },
 });
 
-const { value, errorMessage } = useField(() => props.name);
+const { value, errorMessage } = useField<string>(() => props.name);
 </script>
 
 <template>
   <div class="relative">
-    <Password
-      panelClass="dark:bg-background-dark dark:text-white"
-      :id="id"
-      :name="name"
-      v-model="value as string"
-      toggleMask
-      class="w-full"
-      :feedback="feedback"
+    <Password panelClass="dark:bg-background-dark dark:text-white" :id="id" :name="name" v-model="value as string"
+      toggleMask class="w-full" :feedback="feedback"
       inputClass="block rounded-lg px-2.5 pb-1 pt-4 w-[100%] text-md text-text dark:text-white bg-input border-2 border-border focus:outline-none focus:ring-1 dark:bg-input-dark"
-      :prompt-label="$t('form.input.password.label.prompt')"
-      :weak-label="$t('form.input.password.label.weak')"
-      :medium-label="$t('form.input.password.label.medium')"
-      :strong-label="$t('form.input.password.label.strong')"
-      @focus="handleFocus"
-      @focusout="handleBlur"
-      @input="handleFocus"
-      @date-select="handleFocus"
-      @clear-click="handleBlur"
-      @hide="handleBlur"
-      :pt="{
-        input: { class: 'font-medium' },
-      }"
-    >
+      :prompt-label="$t('form.input.password.label.prompt')" :weak-label="$t('form.input.password.label.weak')"
+      :medium-label="$t('form.input.password.label.medium')" :strong-label="$t('form.input.password.label.strong')"
+      @focus="handleFocus" @focusout="handleBlur" @input="handleFocus" @date-select="handleFocus"
+      @clear-click="handleBlur" @hide="handleBlur" :pt="{
+      input: { class: 'font-medium' },
+    }">
       <template #footer>
         <Divider type="solid" class="text-input-placeholder border border-10" />
         <p class="mt-2">
@@ -69,38 +55,32 @@ const { value, errorMessage } = useField(() => props.name);
           <li>
             <T keyName="form.input.password.prompt.suggestion.uppercase" />
           </li>
-          <li><T keyName="form.input.password.prompt.suggestion.numeric" /></li>
           <li>
-            <T
-              keyName="form.input.password.prompt.suggestion.minimum.characters"
-            />
+            <T keyName="form.input.password.prompt.suggestion.numeric" />
+          </li>
+          <li>
+            <T keyName="form.input.password.prompt.suggestion.minimum.characters" />
           </li>
         </ul>
       </template>
     </Password>
     <div class="h-3 text-left">
-      <span
-        class="ml-3 text-error dark:text-error-dark dark:font-bold text-left text-xs"
-        >{{ errorMessage }}</span
-      >
+      <span class="ml-3 text-error dark:text-error-dark dark:font-bold text-left text-xs">{{ errorMessage }}</span>
     </div>
-    <label
-      :for="id"
-      class="absolute text-sm top-0 left-0 pl-3 py-4 pointer-events-none transition-all duration-300"
+    <label :for="id" class="absolute text-sm top-0 left-0 pl-3 py-4 pointer-events-none transition-all duration-300"
       :class="{
-        'text-input-placeholder': !isFocused,
-        'text-input-label': isFocused,
-        '-translate-y-4  scale-75': isFocused || value,
-        'translate-y-0 scale-100': !isFocused && !value,
-        '-translate-x-2':
-          (feedbackStyle && isFocused) || (feedbackStyle && value),
-        '-translate-x-3.5':
-          (!feedbackStyle && isFocused) || (!feedbackStyle && value),
-        'pl-1.5':
-          (!feedbackStyle && isFocused && german) ||
-          (!feedbackStyle && value && german),
-      }"
-    >
+      'text-input-placeholder': !isFocused,
+      'text-input-label': isFocused,
+      '-translate-y-4  scale-75': isFocused || value,
+      'translate-y-0 scale-100': !isFocused && !value,
+      '-translate-x-2':
+        (feedbackStyle && isFocused) || (feedbackStyle && value),
+      '-translate-x-3.5':
+        (!feedbackStyle && isFocused) || (!feedbackStyle && value),
+      'pl-1.5':
+        (!feedbackStyle && isFocused && german) ||
+        (!feedbackStyle && value && german),
+    }">
       <T :keyName="translationKey" />
     </label>
   </div>
