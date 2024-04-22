fe72d1673415c7736da8490480592ec3ac988b72
Severin Rosner <einsevspielt@gmail.com>
2024-04-22 22:29:43 +0200

    JP-13: fix: run prettier

--- frontend/stores/journey.js
+++ frontend/stores/journey.js
@@ -1,20 +1,20 @@
-import { ref } from "vue";
 import { defineStore } from "pinia";
+import { ref } from "vue";
 
 export const useJourneyStore = defineStore("journey", () => {
-  const journey = ref({});
+    const journey = ref({});
 
-  function setJourney(journey) {
-    this.journey = journey;
-  }
+    function setJourney(journey) {
+        this.journey = journey;
+    }
 
-  function getFromDate() {
-    return this.journey.from;
-  }
+    function getFromDate() {
+        return this.journey.from;
+    }
 
-  function getToDate() {
-    return this.journey.to;
-  }
+    function getToDate() {
+        return this.journey.to;
+    }
 
-  return { journey, setJourney, getFromDate, getToDate };
+    return { journey, setJourney, getFromDate, getToDate };
 });
