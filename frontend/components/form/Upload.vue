e55b065c4b05d9ba32b734946910b27a8ce61f20
Severin Rosner <einsevspielt@gmail.com>
2024-04-30 19:14:24 +0200

    JP-18: feat: uppy demo

---
+++ frontend/components/form/Upload.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Uppy from "@uppy/core";
+import "@uppy/core/dist/style.css";
+import "@uppy/dashboard/dist/style.css";
+import Tus from "@uppy/tus";
+import { Dashboard } from "@uppy/vue";
+
+const uppy = new Uppy().use(Tus, {
+    endpoint: "https://tusd.tusdemo.net/files/",
+});
+</script>
+
+<template>
+    <Dashboard :uppy="uppy" class="" />
+</template>
