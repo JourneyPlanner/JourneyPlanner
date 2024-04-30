20e2cc026034fc96e46c14bee8f0f47f75b36181
Severin Rosner <einsevspielt@gmail.com>
2024-04-30 17:30:11 +0200

    JP-23: feat: add imprint and policy to footer

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -4,13 +4,21 @@
             <slot class="" />
         </main>
         <footer
-            class="mb-auto mt-auto flex w-full justify-center py-2 font-nunito text-xs text-footer md:text-sm"
+            class="mb-auto mt-auto w-full flex-wrap justify-center py-2 text-center font-nunito text-xs text-footer md:text-sm"
         >
+            <NuxtLink to="/imprint" class="hover:font-bold hover:text-link">
+                <T key-name="common.imprint" />
+            </NuxtLink>
+            <span class="mx-2"> | </span>
             <a
                 href="mailto:contact@journeyplanner.io"
                 class="hover:font-bold hover:text-link"
                 >contact@journeyplanner.io</a
             >
+            <span class="mx-2"> | </span>
+            <NuxtLink to="/privacy" class="hover:font-bold hover:text-link">
+                <T key-name="common.privacypolicy" />
+            </NuxtLink>
             <span class="mx-2"> | </span>
             <a
                 href="https://github.com/JourneyPlanner/JourneyPlanner"
