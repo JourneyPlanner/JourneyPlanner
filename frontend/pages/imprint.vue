cf3002247bde00240dc864ca6a6f3b81f0b788a0
Severin Rosner <einsevspielt@gmail.com>
2024-05-13 18:50:53 +0200

    JP-23: fix: responsiveness of imprint

--- frontend/pages/imprint.vue
+++ frontend/pages/imprint.vue
@@ -8,18 +8,18 @@ const { t } = useTranslate();
     <div>
         <div class="flex justify-end">
             <button
-                class="mr-32 mt-10 rounded-xl border-2 border-cta-border bg-input px-2 py-1 text-base font-semibold hover:bg-cta-bg dark:bg-input-dark dark:text-input dark:hover:bg-cta-bg-dark"
+                class="mr-10 mt-5 rounded-xl border-2 border-cta-border bg-input px-2 py-1 text-base font-semibold hover:bg-cta-bg dark:bg-input-dark dark:text-input dark:hover:bg-cta-bg-dark"
                 @click="$router.back()"
             >
                 <T key-name="common.back" />
             </button>
         </div>
-        <div class="-mt-10 font-nunito text-text dark:text-input">
-            <div class="mx-5 mt-10 flex flex-col sm:mx-20 lg:mx-96">
+        <div class="-mt-8 font-nunito text-text dark:text-input">
+            <div class="mx-5 mt-10 flex flex-col sm:mx-20 lg:mx-52 xl:mx-96">
                 <h4 class="text-center text-3xl">
                     <T key-name="common.imprint" />
                 </h4>
-                <p class="mt-2">
+                <p class="mt-4">
                     <T key-name="imprint.info" />
                 </p>
                 <p>
@@ -34,7 +34,7 @@ const { t } = useTranslate();
                     <b> <T key-name="imprint.contact.title" />: </b> <br />
                     <T key-name="imprint.contact.phone.title" />:
                     <a
-                        class="hover:underline"
+                        class="hover:text-link hover:underline"
                         :href="'tel:' + t('imprint.contact.phone.value')"
                     >
                         <T key-name="imprint.contact.phone.value" />
@@ -42,7 +42,7 @@ const { t } = useTranslate();
                     <br />
                     <T key-name="imprint.contact.email.title" />:
                     <a
-                        class="hover:underline"
+                        class="hover:text-link hover:underline"
                         :href="'mailto:' + t('imprint.contact.email.value')"
                     >
                         <T key-name="imprint.contact.email.value" />
@@ -53,7 +53,7 @@ const { t } = useTranslate();
                     <b> <T key-name="imprint.legislation.title" />: </b>
                     <a
                         :href="t('imprint.legislation.value')"
-                        class="hover:underline"
+                        class="hover:text-link hover:underline"
                     >
                         <T key-name="imprint.legislation.value" />
                     </a>
@@ -63,7 +63,7 @@ const { t } = useTranslate();
                     <T key-name="imprint.dispute.value" />:
                     <a
                         :href="t('imprint.dispute.link')"
-                        class="hover:underline"
+                        class="hover:text-link hover:underline"
                     >
                         <T key-name="imprint.dispute.link" />
                     </a>
