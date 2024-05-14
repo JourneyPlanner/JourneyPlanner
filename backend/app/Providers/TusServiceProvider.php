5c706547583f1430a1a858d60541086824f93fe0
Raven Burkard <mathematti@outlook.com>
2024-05-14 16:41:15 +0200

    JP-18: fix: Properly configure tus deployment

--- backend/app/Providers/TusServiceProvider.php
+++ backend/app/Providers/TusServiceProvider.php
@@ -16,6 +16,7 @@ class TusServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton("tus-server", function ($app) {
+            \TusPhp\Config::set("config/tus.php");
             $server = new TusServer("redis");
 
             $server
