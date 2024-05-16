642569e07c5766eaf6f5ce9342153a9558a2ad4a
Severin Rosner <einsevspielt@gmail.com>
2024-05-16 22:57:28 +0200

    JP-23: fix: still broken links and spacing

--- frontend/pages/imprint.vue
+++ frontend/pages/imprint.vue
@@ -2,25 +2,25 @@
     <div>
         <div class="flex justify-end">
             <button
-                class="mr-10 mt-5 rounded-xl border-2 border-cta-border bg-input px-2 py-1 text-base font-semibold hover:bg-cta-bg dark:bg-input-dark dark:text-input dark:hover:bg-cta-bg-dark"
+                class="mr-2 mt-5 rounded-xl border-2 border-cta-border bg-input px-2 py-1 text-base font-semibold hover:bg-cta-bg dark:bg-input-dark dark:text-input dark:hover:bg-cta-bg-dark md:mr-10 md:px-6"
                 @click="$router.back()"
             >
                 <T key-name="common.back" />
             </button>
         </div>
         <div class="-mt-8 font-nunito text-text dark:text-input">
-            <div class="mx-5 mt-10 flex flex-col sm:mx-20 lg:mx-52 xl:mx-96">
-                <h4 class="text-center text-3xl font-bold">
+            <div class="mx-5 mt-7 flex flex-col sm:mx-20 lg:mx-52 xl:mx-96">
+                <h4 class="mb-2 text-center text-3xl font-bold">
                     <T key-name="common.imprint" />
                 </h4>
-                <p class="mt-4">
+                <p class="mt-1">
                     <T key-name="imprint.info" />
                 </p>
-                <p>
+                <p class="mt-1">
                     <b> <T key-name="imprint.operator.title" />: </b>
                     <T key-name="imprint.operator.value" />
                 </p>
-                <p>
+                <p class="mt-1">
                     <b> <T key-name="imprint.address.title" />: </b>
                     <T key-name="imprint.address.value" />
                 </p>
@@ -31,7 +31,7 @@
                         class="hover:text-link hover:underline"
                         href="tel:+43 681 20301077"
                     >
-                        <T key-name="imprint.contact.phone.value" />
+                        +43 681 20301077
                     </a>
                     <br />
                     <T key-name="imprint.contact.email.title" />:
@@ -39,36 +39,38 @@
                         class="hover:text-link hover:underline"
                         href="mailto:raven.burkard@journeyplanner.io"
                     >
-                        <T key-name="imprint.contact.email.value" />
+                        raven.burkard@journeyplanner.io
                     </a>
                 </p>
 
-                <p>
+                <p class="mt-1">
                     <b> <T key-name="imprint.legislation.title" />: </b>
                     <a
-                        href="www.ris.bka.gv.at"
+                        href="https://www.ris.bka.gv.at"
                         target="_blank"
                         class="hover:text-link hover:underline"
                     >
-                        <T key-name="www.ris.bka.gv.at" />
+                        https://www.ris.bka.gv.at
                     </a>
                 </p>
-                <p>
+                <p class="mt-1">
                     <b> <T key-name="imprint.dispute.title" />: </b><br />
                     <T key-name="imprint.dispute.value" />:
                     <a
-                        href="http://ec.europa.eu/odr"
+                        href="https://ec.europa.eu/odr"
                         target="_blank"
                         class="hover:text-link hover:underline"
-                        >http://ec.europa.eu/odr
+                        >https://ec.europa.eu/odr
                     </a>
                 </p>
-                <p>
+                <p class="mt-1">
                     <b> <T key-name="imprint.disclaimer.title" />: </b><br />
                     <T key-name="imprint.disclaimer.value" />
                 </p>
-                <p>
-                    <b> <T key-name="imprint.purpose.title" /> </b>:
+                <p class="mt-1">
+                    <b>
+                        <T key-name="imprint.purpose.title" /> </b
+                    >:
                     <T key-name="imprint.purpose.value" />
                 </p>
                 <p class="mt-3">
