25e02b8f32ae0446a7bd7cbd09e10beb13dcc4be
Severin Rosner <einsevspielt@gmail.com>
2024-06-05 23:32:33 +0200

    JP-9: feat: responsive starpage

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -25,23 +25,25 @@ const changeColorMode = () => {
     <div
         class="relative cursor-default overflow-hidden font-nunito text-text dark:text-natural-50"
     >
-        <div class="mb-72 min-h-56 md:min-h-72 xl:mb-96 xl:min-h-[40rem]">
-            <div class="">
+        <div class="mb-52 min-h-20 sm:mb-80 md:mb-72 xl:min-h-[40rem]">
+            <div>
                 <SvgStartpagePlane
-                    class="absolute -right-40 top-40 -z-10 w-[170%] sm:-right-48 md:top-16 md:w-[110%] lg:-right-20 lg:top-10 lg:w-full"
+                    class="absolute -right-28 top-48 -z-10 sm:-right-48 sm:top-20 sm:w-[130%] md:top-16 md:w-[110%] lg:-right-20 lg:top-10 lg:w-full"
                 />
             </div>
             <header
-                class="relative z-10 mx-5 mt-3 flex flex-row justify-between text-base font-medium text-text"
+                class="relative z-10 mx-5 mt-3 flex flex-row items-center justify-between text-base font-medium text-text"
             >
                 <NuxtLink to="/">
-                    <h3 class="font-bold text-calypso-400 dark:text-gothic-400">
+                    <h3
+                        class="text-sm font-bold text-calypso-400 dark:text-gothic-400 sm:text-base"
+                    >
                         JourneyPlanner
                     </h3>
                 </NuxtLink>
                 <nav class="flex flex-row gap-4">
                     <ul
-                        class="flex flex-row gap-4 text-natural-500 dark:text-natural-600"
+                        class="hidden flex-row items-center gap-4 text-natural-500 dark:text-natural-600 md:flex"
                     >
                         <li>
                             <NuxtLink
@@ -64,9 +66,14 @@ const changeColorMode = () => {
                             </NuxtLink>
                         </li>
                     </ul>
-                    <ul class="flex flex-row gap-4 pl-5">
+                    <ul
+                        class="flex flex-row items-center gap-2 md:gap-4 md:pl-5"
+                    >
                         <li class="flex">
-                            <button class="group" @click="changeColorMode()">
+                            <button
+                                class="group hidden md:flex"
+                                @click="changeColorMode()"
+                            >
                                 <SvgMoon
                                     v-if="icon === 'moon'"
                                     class="group w-4"
@@ -77,7 +84,7 @@ const changeColorMode = () => {
                         <li class="border-l-2 border-natural-300 pl-2">
                             <NuxtLink
                                 to="/login"
-                                class="text-text hover:underline dark:text-natural-50"
+                                class="text-sm text-text hover:underline dark:text-natural-50 md:text-base"
                             >
                                 <T key-name="form.button.login" />
                             </NuxtLink>
@@ -85,7 +92,7 @@ const changeColorMode = () => {
                         <li>
                             <NuxtLink
                                 to="/register"
-                                class="rounded-lg border-2 border-dandelion-300 p-1 text-text hover:bg-dandelion-200 dark:text-natural-50 dark:hover:bg-pesto-600"
+                                class="rounded-lg border-2 border-dandelion-300 p-1 text-sm text-text hover:bg-dandelion-200 dark:text-natural-50 dark:hover:bg-pesto-600 md:text-base"
                             >
                                 <T key-name="form.button.register" />
                             </NuxtLink>
@@ -93,59 +100,69 @@ const changeColorMode = () => {
                     </ul>
                 </nav>
             </header>
-            <div class="ml-5 mt-14 lg:ml-20 lg:mt-24">
-                <h1 class="text-3xl font-bold lg:text-5xl">JourneyPlanner</h1>
+            <div class="ml-5 mt-8 md:mt-14 lg:ml-20 lg:mt-24">
+                <h1 class="text-3xl font-bold sm:text-4xl lg:text-5xl">
+                    JourneyPlanner
+                </h1>
                 <h2
-                    class="mt-1 text-xl font-bold text-calypso-500 dark:text-gothic-400 lg:text-3xl"
+                    class="mt-1 text-xl font-bold text-calypso-500 dark:text-gothic-400 sm:text-2xl lg:text-3xl"
                 >
                     <T key-name="startpage.subheading" />
                 </h2>
-                <p class="mt-2 w-64 text-base md:w-80 lg:w-96 lg:text-lg">
+                <p class="mt-2 w-64 text-base sm:w-96 md:text-lg">
                     <T key-name="startpage.text" />
                 </p>
             </div>
         </div>
-        <div id="features" class="mb-20 pt-14">
+        <div id="features" class="mb-20 pt-5 md:pt-10">
             <div class="relative flex flex-row">
                 <div class="w-[40rem] max-lg:hidden">
-                    <SvgStartpagePeople />
+                    <SvgStartpagePeople class="mt-10" />
                 </div>
-                <div class="max-lg:w-full lg:ml-20">
+                <div
+                    class="flex flex-col max-lg:w-full sm:flex-row sm:justify-center sm:gap-6 lg:ml-20 lg:w-auto lg:flex-col"
+                >
                     <div class="flex flex-col max-lg:items-center lg:flex-row">
                         <SvgStartpageCalendar
                             class="w-24 hover:animate-wiggle"
                         />
                         <div class="flex flex-col max-lg:text-center lg:ml-4">
-                            <h4 class="text-2xl font-semibold">
+                            <h4 class="text-xl font-semibold md:text-2xl">
                                 <T key-name="startpage.calendar.header" />
                             </h4>
-                            <p class="mt-0.5 w-[30rem] text-lg">
+                            <p
+                                class="mt-0.5 w-[15rem] text-base sm:w-[12.5rem] md:text-lg lg:w-[30rem]"
+                            >
                                 <T key-name="startpage.calendar.text" />
                             </p>
                         </div>
                     </div>
                     <div
-                        class="mt-16 flex flex-col max-lg:items-center lg:ml-16 lg:flex-row"
+                        class="mt-8 flex flex-col max-lg:items-center md:mt-16 lg:ml-16 lg:flex-row"
                     >
                         <SvgStartpageShare class="w-24 hover:animate-wiggle" />
-                        <div class="ml-4 flex flex-col max-lg:text-center">
+                        <div class="flex flex-col max-lg:text-center lg:ml-4">
                             <h4 class="text-2xl font-semibold">
                                 <T key-name="startpage.share.header" />
                             </h4>
-                            <p class="mt-0.5 w-[30rem] text-lg">
+                            <p
+                                class="mt-0.5 w-[15rem] text-base sm:w-[12.5rem] md:text-lg lg:w-[30rem]"
+                            >
                                 <T key-name="startpage.share.text" />
                             </p>
                         </div>
                     </div>
                     <div
-                        class="mt-16 flex flex-col max-lg:items-center lg:ml-32 lg:flex-row"
+                        class="mt-8 flex flex-col max-lg:items-center md:mt-0 lg:ml-32 lg:mt-16 lg:flex-row"
                     >
                         <SvgStartpageGlobe class="w-24 hover:animate-wiggle" />
-                        <div class="ml-4 flex flex-col max-lg:text-center">
+                        <div class="flex flex-col max-lg:text-center lg:ml-4">
                             <h4 class="text-2xl font-semibold">
                                 <T key-name="startpage.experience.header" />
                             </h4>
-                            <p class="mt-0.5 w-[30rem] text-lg">
+                            <p
+                                class="mt-0.5 w-[15rem] text-base sm:w-[12.5rem] md:text-lg lg:w-[30rem]"
+                            >
                                 <T key-name="startpage.experience.text" />
                             </p>
                         </div>
@@ -155,7 +172,7 @@ const changeColorMode = () => {
         </div>
         <div id="team" class="mb-20">
             <ScrollPanel>
-                <div class="flex items-center justify-center">
+                <div class="flex items-center justify-center font-nunito">
                     <div
                         class="align-center flex w-full items-center gap-4 lg:grid lg:grid-cols-4"
                     >
