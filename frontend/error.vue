ca0d77ee9e8c3b1dfd96c2080ccb7fb50739f22b
Severin Rosner <einsevspielt@gmail.com>
2024-06-17 19:36:28 +0200

    fix: back button and dark mode style

--- frontend/error.vue
+++ frontend/error.vue
@@ -14,19 +14,29 @@ const handleError = () => {
 
 <template>
     <NuxtLayout>
-        <div class="mt-16 flex flex-col items-center">
+        <div
+            class="mt-16 flex flex-col items-center text-text dark:text-natural-50"
+        >
             <h1 class="text-xl">Error</h1>
             <p>
                 <strong>{{ error?.message }}</strong>
             </p>
             <p>
-                <button class="m-3 rounded-lg border p-1" @click="handleError">
+                <button
+                    class="m-3 rounded-lg border-2 border-dandelion-300 p-1 px-2 text-sm text-text hover:bg-dandelion-200 dark:text-natural-50 dark:hover:bg-pesto-600 md:text-base"
+                    @click="handleError"
+                >
                     Back to dashboard
                 </button>
             </p>
             <span class="text-sm">--- Site is still in development ---</span>
-            <a class="text-xs" href="mailto:contact@journeyplanner.io"
-                >Report issues to: contact@journeyplanner.io</a
+            <span class="font-nunito text-xs text-natural-400"
+                >Report issues to:
+                <a
+                    href="mailto:contact@journeyplanner.io"
+                    class="hover:font-bold hover:text-calypso-400"
+                    >contact@journeyplanner.io</a
+                ></span
             >
         </div>
     </NuxtLayout>
