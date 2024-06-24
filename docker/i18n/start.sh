5027035f232a1a59d58a9c5849563b03d26ad00f
Raven Burkard <77683178+Mathematti@users.noreply.github.com>
2024-06-24 15:34:21 +0200

    fix: Correctly fetch translations from tolgee

--- docker/i18n/start.sh
+++ docker/i18n/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 printenv | sed 's/^\(.*\)$/export \1/g' > /app/project_env.sh
-echo "tolgee pull -o /app/i18n --project-id $TOLGEE_PROJECT_ID -au $TOLGEE_API_URL" > /app/pull_translations.sh
+echo "tolgee pull --path /app/i18n --project-id $TOLGEE_PROJECT_ID -au $TOLGEE_API_URL" > /app/pull_translations.sh
 chmod +x /app/pull_translations.sh
 service cron start
 nginx -g 'daemon off;'
