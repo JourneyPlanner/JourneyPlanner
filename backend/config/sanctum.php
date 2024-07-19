6411fb3e37be2d3c171a3344dbe13b79fc0ee656
Raven Burkard <mathematti@outlook.com>
2024-07-19 10:07:33 +0200

    fix: Run prettier

--- backend/config/sanctum.php
+++ backend/config/sanctum.php
@@ -67,9 +67,11 @@ return [
     |
     */
 
-    'middleware' => [
-        'authenticate_session' => Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
-        'encrypt_cookies' => Illuminate\Cookie\Middleware\EncryptCookies::class,
-        'validate_csrf_token' => Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
+    "middleware" => [
+        "authenticate_session" =>
+            Laravel\Sanctum\Http\Middleware\AuthenticateSession::class,
+        "encrypt_cookies" => Illuminate\Cookie\Middleware\EncryptCookies::class,
+        "validate_csrf_token" =>
+            Illuminate\Foundation\Http\Middleware\ValidateCsrfToken::class,
     ],
 ];
