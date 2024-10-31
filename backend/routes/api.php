4f248ee070eb7225f9aabb312cdf885399ec65e2
Raven Burkard <mathematti@outlook.com>
2024-10-31 11:30:50 +0100

    JP-7: fix: Adjust route for retrieving the current user

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -23,7 +23,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(["auth:sanctum"])->get("/user", function (Request $request) {
+Route::middleware(["auth:sanctum"])->get("/me", function (Request $request) {
     return $request->user();
 });
 
