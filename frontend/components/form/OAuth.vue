aac45d0724836287778b61fb8178f60ea000dccf
Severin Rosner <einsevspielt@gmail.com>
2024-11-15 12:56:49 +0100

    JP-92: fix: responsive padding on divider line

--- frontend/components/form/OAuth.vue
+++ frontend/components/form/OAuth.vue
@@ -1,10 +1,10 @@
 <template>
-    <div class="flex w-full items-center px-10">
-        <span class="h-[1.6px] w-2/3 bg-calypso-300 dark:bg-calypso-400" />
+    <div class="flex w-full items-center px-1 xs:px-12 md:px-1 lg:px-10">
+        <span class="h-[1.6px] w-full bg-calypso-300 dark:bg-calypso-400" />
         <span class="mx-2 text-lg font-bold text-text dark:text-natural-50"
             ><T key-name="common.or"
         /></span>
-        <span class="h-[1.6px] w-2/3 bg-calypso-300 dark:bg-calypso-400" />
+        <span class="h-[1.6px] w-full bg-calypso-300 dark:bg-calypso-400" />
     </div>
     <div class="flex flex-col justify-center gap-y-3">
         <FormGoogleButton />
