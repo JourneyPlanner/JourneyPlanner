b715d2d6cb88759aacd4411331122568478c1ac3
Severin Rosner <einsevspielt@gmail.com>
2024-12-04 20:17:57 +0100

    JP-7: refactor: remove console.log

--- frontend/utils/debounce.ts
+++ frontend/utils/debounce.ts
@@ -4,8 +4,6 @@ export default function debounce<T extends (...args: unknown[]) => unknown>(
 ) {
     let timeoutId: ReturnType<typeof setTimeout>;
     return function (this: ThisParameterType<T>, ...args: Parameters<T>): void {
-        console.log(args);
-
         clearTimeout(timeoutId);
         timeoutId = setTimeout(() => fn.apply(this, args), ms);
     };
