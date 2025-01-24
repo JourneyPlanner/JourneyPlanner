741c7a52b3ec2c4f04742feb15afc4427dbae03f
Raven Burkard <mathematti@outlook.com>
2025-01-24 22:47:43 +0100

    JP-110: feat: Add API documentation

--- backend/app/Http/Controllers/Journey/Activity/ActivityController.php
+++ backend/app/Http/Controllers/Journey/Activity/ActivityController.php
@@ -105,6 +105,7 @@ class ActivityController extends Controller
 
     /**
      * Update the specified activity in storage.
+     *
      * Do not remove the journey parameter, it is required for authorization.
      */
     public function update(
--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -3,8 +3,11 @@
 namespace App\Providers;
 
 use App\Mail\EmailConfirmation;
+use Dedoc\Scramble\Scramble;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Auth\Notifications\VerifyEmail;
+use Illuminate\Routing\Route;
+use Illuminate\Support\Str;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +26,17 @@ class AppServiceProvider extends ServiceProvider
     {
         VerifyEmail::toMailUsing(function (object $notifiable, string $url) {
             return (new EmailConfirmation($url))->to($notifiable->email);
+        });
+
+        Scramble::routes(function (Route $route) {
+            return Str::startsWith($route->uri, "api/") ||
+                Str::startsWith($route->uri, "auth/") ||
+                Str::startsWith($route->uri, "email/") ||
+                Str::startsWith($route->uri, "logout") ||
+                Str::startsWith($route->uri, "register") ||
+                Str::startsWith($route->uri, "login") ||
+                Str::startsWith($route->uri, "forgot-password") ||
+                Str::startsWith($route->uri, "reset-password");
         });
     }
 }
