b0c65f4601baf86fc84abc8bc0451bc45fbb8fb1
Raven Burkard <mathematti@outlook.com>
2025-01-28 16:44:29 +0100

    JP-110: feat: Basic scribe + scalar setup

---
+++ backend/resources/views/scribe/index.blade.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<head>
+    <title>JourneyPlanner Documentation</title>
+    <meta charset="utf-8"/>
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"/>
+    <style>
+        body {
+            margin: 0;
+        }
+    </style>
+</head>
+<body>
+
+<script
+    id="api-reference"
+    
+    data-configuration="{&quot;theme&quot;: &quot;purple&quot;}"
+    data-url="{{ route("scribe.openapi") }}">
+</script>
+<script src="https://cdn.jsdelivr.net/npm/@scalar/api-reference"></script>
+</body>
+</html>
