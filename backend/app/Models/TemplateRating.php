c339e80ca30acbabe2173d47e8dbcaa94eab1341
Raven Burkard <mathematti@outlook.com>
2025-01-29 14:09:12 +0100

    JP-25: feat: Backend for rating templates

---
+++ backend/app/Models/TemplateRating.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TemplateRating extends Model
+{
+    use HasFactory, HasUuids;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = ["user_id", "template_id", "rating"];
+
+    /**
+     * Bootstrap the model and its traits.
+     *
+     * @return void
+     */
+    public static function boot()
+    {
+        parent::boot();
+
+        static::saved(function ($rating) {
+            $rating->updateTemplateRating();
+        });
+
+        static::deleted(function ($rating) {
+            $rating->updateTemplateRating();
+        });
+    }
+
+    /**
+     * Update the average rating and total ratings of the template.
+     */
+    public function updateTemplateRating()
+    {
+        $template = Journey::find($this->template_id);
+
+        $average = TemplateRating::where(
+            "template_id",
+            $this->template_id
+        )->avg("rating");
+        $count = TemplateRating::where(
+            "template_id",
+            $this->template_id
+        )->count();
+
+        $template->update([
+            "average_rating" => $average,
+            "total_ratings" => $count,
+        ]);
+    }
+
+    /**
+     * Get the user that rated the template.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the template that was rated.
+     */
+    public function template(): BelongsTo
+    {
+        return $this->belongsTo(Journey::class);
+    }
+}
