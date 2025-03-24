3696275d17d848ab5db2aff2257c6a2dd55d3cf5
Ixpiria <roman.krebs@gmx.at>
2025-03-24 11:03:40 +0100

    JP-210: fix: adds bugfix

--- frontend/components/journey/id/JourneyCalendar.vue
+++ frontend/components/journey/id/JourneyCalendar.vue
@@ -321,6 +321,7 @@ const calendarOptions = reactive({
     eventDrop: initializeDrop,
     eventResize: editDrop,
     eventClick: showData,
+    editable: props.currentUserRole === 1,
     eventBackgroundColor: bg,
     eventBorderColor: border,
     eventTextColor: text,
@@ -330,7 +331,6 @@ const calendarOptions = reactive({
     droppable: true,
     initialDate: start,
     firstDay: 1,
-    editable: true,
     views: {
         fullweek: {
             type: "timeGrid",
@@ -805,6 +805,7 @@ function call(editType: string) {
 .fc-timegrid-event.fc-event-mirror,
 .fc-timegrid-more-link {
     box-shadow: none;
+    cursor: pointer;
 }
 
 .dark .fc-theme-standard td,
